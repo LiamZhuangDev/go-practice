e1181105dcbebca154c93c2ed3a7ebf2cbdb8b8f
Liang Zhuang <bzhuangliang@gmail.com>
2026-01-22 20:04:50 -0800

    creates a simple calculator

---
+++ basics/calculator.go
@@ -0,0 +1,27 @@
+package calculator
+
+import (
+	"errors"
+)
+
+type Calculator struct {
+}
+
+func (c Calculator) Add(a, b float64) float64 {
+	return a + b
+}
+
+func (c Calculator) Subtract(a, b float64) float64 {
+	return a - b
+}
+
+func (c Calculator) Multiply(a, b float64) float64 {
+	return a * b
+}
+
+func (c Calculator) Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("division by zero is not allowed")
+	}
+	return a / b, nil
+}
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	calculator "go-practice/basics"
+)
+
+func main() {
+	calc := calculator.Calculator{}
+
+	sum := calc.Add(10, 5)
+	fmt.Printf("10 + 5 = %.2f\n", sum)
+
+	diff := calc.Subtract(10, 5)
+	fmt.Printf("10 - 5 = %.2f\n", diff)
+
+	product := calc.Multiply(10, 5)
+	fmt.Printf("10 * 5 = %.2f\n", product)
+
+	quotient, err := calc.Divide(10, 5)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("10 / 5 = %.2f\n", quotient)
+	}
+
+	quotient, err = calc.Divide(10, 0)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("10 / 0 = %.2f\n", quotient)
+	}
+}
